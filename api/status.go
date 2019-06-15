96bf170d6a0a0894aecc6b04139c951ba04bc549
Rahul Guha <rahul@rahuls-mbp.lan>
2019-06-14 20:09:15 -0500

    added api folder

--- api/status.go
+++ api/status.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type SystemHealth struct {
+	ID     string  "json:'id'"
+	CPU    float32 "json:'cpu'"
+	Memory float32 "json:'memory'"
+}
+type SystemHealthDataPoints []SystemHealth
+
+func systemHealthData(w http.ResponseWriter, r *http.Request) {
+	// Mock SystemHealth for now
+	systemHealthDataPoints := SystemHealthDataPoints{
+		SystemHealth{ID: "A1234", CPU: 23.65, Memory: 5.25},
+		SystemHealth{ID: "A1235", CPU: 25.65, Memory: 5.75},
+	}
+	json.NewEncoder(w).Encode(systemHealthDataPoints)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "I am Home !")
@@ -16,6 +33,7 @@ func ping(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/systemhealth", systemHealthData)
 	log.Fatal(http.ListenAndServe(":4050", nil))
 }
 
